da6a4665482103612609ad2bd7826ae8e513e517
TsT <tst2005@gmail.com>
2017-07-06 00:23:19 +0200

    improvement

--- ast2sh.lua
+++ ast2sh.lua
@@ -19,7 +19,7 @@ local function squotestring(s)
 		end
 	end).."'"
 end
---[[
+
 local function table_concatlike(self, t, sep)
 	local r = {}
 	for i,v in ipairs(t) do
@@ -31,7 +31,7 @@ local function table_concatlike(self, t, sep)
 	end
 	return table.concat(r, sep or "")
 end
-]]--
+
 
 local sh = x:defs()
 
@@ -48,11 +48,30 @@ function sh:Program(t)
 end
 
 -- CompoundList = 'cmds',
+function sh:CompoundList(t)
+end
+
 -- SequentialList = 'cmds',
+function sh:SequentialList(t)
+end
+
 -- AndList = 'cmds',
+function sh:AndList(t)
+end
+
 -- OrList = 'cmds',
+function sh:OrList(t)
+end
+
 -- Not = 'cmd',
+function sh:Not(t)
+end
+
 -- PipeSequence = 'cmds',
+function sh:PipeSequence(t)
+	return table_concatlike(self, t.cmds, "|")
+end
+
 -- SimpleCommand = { 'prefix', 'cmd', 'suffix' },
 function sh:SimpleCommand(t)
 	local r = {}
@@ -74,28 +93,79 @@ end
 
 -- BraceGroup = { 'body', 'redirs' },
 function sh:BraceGroup(t)
-	return "{\n"..self:render(t.body).."\n}\n" -- redirs
+	return "{\n"..self:render(t.body).."\n}"..table_concatlike(self, t.redirs, " ").."\n"
 end
 
 -- Subshell = { 'body', 'redirs' },
+function sh:Subshell(t)
+	return "( "..self:render(t.body).." )"..table_concatlike(self, t.redirs, " ").."\n"
+end
+
 -- If = { 'clauses', 'redirs' },
+function sh:If(t)
+	return "if "..self:render(t.clauses[1])..table_concatlike(self, t.redirs, " ") -- FIXME: all conditions ?
+end
+
 -- IfClause = { 'cond', 'body' },
+function sh:IfClause(t)
+	return self:render(t.cond)..";then".." "..self:render(t.body)..";".."fi"
+end
+
 -- ElifClause = { 'cond', 'body' },
+function sh:ElifClause(t)
+end
+
 -- ElseClause = { 'body' },
+function sh:ElseClause(t)
+end
+
 -- For = { 'var', 'items', 'body', 'redirs' },
+function sh:For(t)
+end
+
 -- Case = { 'var', 'cases', 'redirs' },
+function sh:Case(t)
+	return "case "..self:render(t.var).." in\n"..table_concatlike(self, t.cases, "\n").."esac\n"
+end
+
 -- CaseItem = { 'pattern', 'body' },
+function sh:CaseItem(t)
+	return "("..self:render(t.pattern)..")" ..(t.body and self:render(t.body) or "")..";;\n"
+end
+
+function sh:Pattern(t)
+	return table_concatlike(self, t.children, "|")
+end
+
 -- While = { 'cond', 'body', 'redirs' },
+function sh:While(t)
+end
+
 -- Until = { 'cond', 'body', 'redirs' },
+function sh:Until(t)
+end
+
 -- FunctionDef = { 'name', 'body', 'redirs' },
 function sh:FunctionDef(t)
 	return self:render(t.name).."()".. self:render(t.body) -- t.redirs
 end
 
 -- RedirectFile = { 'fd', 'op', 'file' },
+function sh:RedirectFile(t)
+	return (t.fd or "")..t.op..self:render(t.file)
+end
+
 -- RedirectHereDoc = { 'fd', 'op', 'delimiter', 'content' },
+function sh:RedirectHereDoc(t)
+end
+
 -- HereDocContent = { 'content' },
+function sh:HereDocContent(t)
+end
+
 -- Assignments = { 'modifier', 'assignments' },
+function sh:Assignments(t)
+end
 
 -- Assignment = { 'name', 'value' },
 function sh:Assignment(t)
@@ -126,9 +196,20 @@ function sh:Word(t)
 end
 
 -- ArithmeticExpansion = { 'text' },
+function sh:ArithmeticExpansion(t)
+end
+
 -- ParameterExpansion = { 'op_pre', 'param', 'op_in', 'word' },
+function sh:ParameterExpansion(t)
+end
+
 -- CommandSubstitution = 'cmds',
+function sh:CommandSubstitution(t)
+end
+
 -- Comment = { 'text' },
+function sh:Comment(t)
+end
 
 function sh:CommandSubBackquote(t)
 	if t.children then
